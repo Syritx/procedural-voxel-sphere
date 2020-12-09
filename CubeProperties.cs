26651818d9ed9557f2f4f579466d37ba5215012f
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-12-09 09:44:56 -0500

    added source

---
+++ CubeProperties.cs
@@ -0,0 +1,100 @@
+using OpenTK.Mathematics;
+using System.Collections.Generic;
+
+namespace _3d {
+    class CubeProperties {
+
+        public static float[] vertices = {
+
+        //   X  Y  Z  R G B
+            -1,-1,-1, 0,1,0, // 0
+            -1, 1,-1, 0,1,0, // 1
+             1, 1,-1, 0,1,0, // 2
+             1,-1,-1, 0,1,0, // 3
+
+            -1,-1, 1, 0,1,0, // 4
+            -1, 1, 1, 0,1,0, // 5
+             1, 1, 1, 0,1,0, // 6
+             1,-1, 1, 0,1,0, // 7
+        };
+        public static uint[] indices = {
+            1, 2, 3, // front
+            3, 0, 1, // front
+
+            5, 6, 7, // back
+            7, 4, 5, // back
+
+            1, 5, 4, // left
+            4, 0, 1, // left
+
+            2, 6, 7, // right
+            7, 3, 2, // right
+
+            1, 2, 6, // top
+            6, 5, 1, // top
+
+            0, 3, 7, // bottom
+            7, 4, 0, // bottom
+        };
+
+        public static List<float> GetPositionedVertices(Vector3 position, List<float> currentVertices) {
+            float[] verts = {
+                -1+position.X,-1+position.Y,-1+position.Z, 0,1,0, // 0
+                -1+position.X, 1+position.Y,-1+position.Z, 0,0,1, // 1
+                 1+position.X, 1+position.Y,-1+position.Z, 0,0,1, // 2
+                 1+position.X,-1+position.Y,-1+position.Z, 0,1,0, // 3
+
+                -1+position.X,-1+position.Y, 1+position.Z, 0,1,0, // 4
+                -1+position.X, 1+position.Y, 1+position.Z, 0,0,1, // 5
+                 1+position.X, 1+position.Y, 1+position.Z, 0,0,1, // 6
+                 1+position.X,-1+position.Y, 1+position.Z, 0,1,0, // 7
+            };
+
+            foreach(float a in verts) {
+                currentVertices.Add(a);
+            }
+
+            return currentVertices;
+        }
+
+        public static List<uint> GetNewIndices(uint position, List<uint> currentIndices) {
+            uint[] inds = {
+                1+(8*position), 2+(8*position), 3+(8*position), // front
+                3+(8*position), 0+(8*position), 1+(8*position), // front
+
+                5+(8*position), 6+(8*position), 7+(8*position), // back
+                7+(8*position), 4+(8*position), 5+(8*position), // back
+
+                1+(8*position), 5+(8*position), 4+(8*position), // left
+                4+(8*position), 0+(8*position), 1+(8*position), // left
+
+                2+(8*position), 6+(8*position), 7+(8*position), // right
+                7+(8*position), 3+(8*position), 2+(8*position), // right
+
+                1+(8*position), 2+(8*position), 6+(8*position), // top
+                6+(8*position), 5+(8*position), 1+(8*position), // top
+
+                0+(8*position), 3+(8*position), 7+(8*position), // bottom
+                7+(8*position), 4+(8*position), 0+(8*position), // bottom
+            };
+
+
+            foreach(uint a in inds) currentIndices.Add(a);
+            return currentIndices;
+        }
+
+        public static List<float> FloatArrayToList(float[] array) {
+            List<float> l = new List<float>();
+
+            foreach(float a in array) l.Add(a*10);
+            return l;
+        }
+
+        public static List<uint> UintArrayToList(uint[] array) {
+            List<uint> l = new List<uint>();
+
+            foreach(uint a in array) l.Add(a);
+            return l;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+
+using OpenTK.Graphics.OpenGL;
+using System.Collections.Generic;
+
+namespace _3d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NativeWindowSettings nws = new NativeWindowSettings() {
+                Title = "Hello",
+                Size = new Vector2i(1000,1000),
+                StartFocused = true,
+                StartVisible = true,
+                APIVersion = new Version(3,2),
+                Flags = ContextFlags.ForwardCompatible,
+                Profile = ContextProfile.Core,
+            };
+            GameWindowSettings gws = new GameWindowSettings();
+            new Game(gws,nws);
+        }
+    }
+
+    class Game : GameWindow {
+
+        Tile[] tiles = new Tile[2];
+        Camera camera;
+
+        List<float> vertices = new List<float>();
+        List<uint> indices = new List<uint>();
+        static int mapResolution = 130, radius = mapResolution/2;
+        double seed = new Random().Next(1,100000);
+
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings,nativeWindowSettings) {
+
+            camera = new Camera(this);
+            seed = new System.Random().Next(1,1000000000);
+
+            int i = 0;
+            for (int x = -(mapResolution/2); x < mapResolution/2; x++) {
+                for (int y = -(mapResolution/2); y < mapResolution/2; y++) {
+                    for (int z = -(mapResolution/2); z < mapResolution/2; z++) {
+
+                        float xNoise = 0, yNoise = 0, zNoise = 0;
+
+                        float nx = (float)x/30,
+                              ny = (float)y/30,
+                              nz = (float)z/30;
+
+                        xNoise = (float)ImprovedNoise.noise(ny*1.1f, nz*1.1f, seed)*20;
+                        yNoise = (float)ImprovedNoise.noise(nx*1.1f, nz*1.1f, seed)*20;
+                        zNoise = (float)ImprovedNoise.noise(nx*1.1f, ny*1.1f, seed)*20;
+
+                        float density = xNoise+yNoise+zNoise;
+
+                        Vector3 position = new Vector3(x,y,z);
+                        float d = Vector3.Distance(position, new Vector3(0,0,0));  
+
+                        if (density < 4 && d < radius) {
+                            vertices = CubeProperties.GetPositionedVertices(new Vector3(x*2,(y*2)-mapResolution,z*2), vertices);
+                            indices = CubeProperties.GetNewIndices((uint)i,indices);
+                            i++;
+                        }
+                    }
+                }
+            }
+            tiles[0] = new Tile(camera, vertices, indices);
+
+            Run();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args) {
+            base.OnRenderFrame(args);
+            tiles[0].Render();
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e) {
+            base.OnResize(e);
+        }
+
+        protected override void OnLoad() {
+            base.OnLoad();
+            GL.Enable(EnableCap.DepthTest);
+            GL.ClearColor(0,0,0,1.0f);
+        }
+    }
+}
